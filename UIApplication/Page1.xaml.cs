18bf7e08559d1fceb9d6605dbe85d08d50a59ef6
Dileepa <astrodileepa@outlook.com>
2017-08-13 17:31:55 +0530

    Working with annimatons !

---
+++ UIApplication/Page1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace UIApplication
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Window
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
